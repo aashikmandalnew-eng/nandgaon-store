88c6104d6a78ea404824603758863a70313a89de
aashikmandalnew-eng <aashikmandalnew@gmail.com>
2026-07-21 00:05:20 +0545

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,5 +1,30 @@
 export default {
-  async fetch(request, env) {
-    return env.ASSETS.fetch(request);
-  },
+  async fetch(request, env, ctx) {
+    return new Response(`
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>NAND GAON SMP STORE</title>
+        <style>
+          body {
+            background:#111;
+            color:white;
+            font-family:Arial;
+            text-align:center;
+            padding:50px;
+          }
+        </style>
+      </head>
+
+      <body>
+        <h1>🔥 NAND GAON SMP STORE 🔥</h1>
+        <p>Store is working!</p>
+      </body>
+      </html>
+    `, {
+      headers:{
+        "content-type":"text/html"
+      }
+    });
+  }
 };
